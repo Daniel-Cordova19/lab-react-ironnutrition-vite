1948c39258b32f5dbfbffd2b8120b72c55fc2049
Daniel Cordova <javierdanielcordova@gmail.com>
2023-06-13 14:07:54 +0200

    listo

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -28,14 +28,19 @@ export default function AddFoodForm({ onCreateFood }) {
 
     return <div>
         <form className="form" onSubmit={handleSubmit}>
+
             <label htmlFor="name">Name:</label>
             <Input id="name" type="text" name="name" maxLength={50} value={name} onChange={(e) => setName(e.target.value)} required></Input>
+
             <label htmlFor="image">Image</label>
             <Input id="image" name="image" type="url" required value={image} onChange={(e) => setImage(e.target.value)}></Input>
+
             <label htmlFor="calories">Calories</label>
             <Input id="calories" name="calories" type="number" min={0} max={10000} value={calories} onChange={(e) => setCalories(e.target.value)} required />
+
             <label htmlFor="servings">Servings</label>
             <Input id="servings" name="servings" type="number" min={1} max={1000} value={servings} onChange={(e) => setServings(e.target.value)} required></Input>
+            
             <Button>Create</Button>
         </form>
 
